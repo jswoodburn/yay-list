b677211d501cf1c35a5087f743d0eb26bf1e0140
Jackie Woodburn <jackie.woodburn@softwire.com>
2021-12-06 17:45:33 +0000

    Added pseudocode for endpoint calls!

--- src/api/api.js
+++ src/api/api.js
@@ -14,6 +14,47 @@ const serialize = (obj) => {
     return str.join("&");
 }
 
+// input sentence from form here
+const createAndPopulateNewPlaylistFromSentence = (sentence) => { // TODO: need authentication here
+    const trackUris = getTrackUrisFromSentence(sentence);
+
+    // create playlist endpoint: https://developer.spotify.com/documentation/web-api/reference/#/operations/create-playlist
+    // TODO: can we add tracks straight here or do we need to call add tracks endpoint here?
+    // either create and add tracks here OR have create method that returns playlist ID and then update method that takes ID and trackUris
+    // add tracks to playlist endpoint: https://developer.spotify.com/documentation/web-api/reference/#/operations/reorder-or-replace-playlists-tracks
+    const playlistId = 1; // temp
+
+    return playlistId
+}
+
+const getTrackUrisFromSentence = (sentence) => {
+    const trackNames = sentence.split(' ');
+    let trackUrisForPlaylist = [];
+    trackNames.forEach(
+        trackName => trackUrisForPlaylist.push(getTrackWithName(trackName))
+    );
+    return trackUrisForPlaylist
+}
+
+const getTrackWithName = (name) => {
+    fetch(base_url + '/v1/search?q=' + name + '&type=track') // TODO: need authentication here
+        .then(response => response.json)
+        .then(searchResults => {
+            for (const searchResult of searchResults) {
+                if (searchResult.name === name) {
+                    return searchResult.uri;
+                }
+            }
+        });
+}
+
+// this is the last thing we call, to get playlist for Results page
+// display playlist info: https://developer.spotify.com/documentation/general/guides/working-with-playlists/
+const getPlaylist = (playlistId) => { // TODO: need authentication here
+    // get playlist for display:
+    // https://developer.spotify.com/documentation/web-api/reference/#/operations/get-playlist
+}
+
 export const loginAddress = 'https://accounts.spotify.com/authorize?' + serialize({
     response_type: 'code',
     client_id: client_id,
