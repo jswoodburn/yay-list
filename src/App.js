07a346ba06b8a9ab9ed44fa6211b240a04630b57
Daniel Holmes <daniel.holmes@softwire.com>
2021-12-06 15:48:32 +0000

    Fix react router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import './App.css';
 import Home from './components/Home';
 import Results from './components/Results';
-import { Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => (
     <div>
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/results">Results</Link>
-        </nav>
-        <Routes>
-            <Route path="/">
-                <Home />
-            </Route>
-            <Route path="/results">
-                <Results />
-            </Route>
-        </Routes>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path="/results" element={<Results />} />
+            </Routes>
+        </BrowserRouter>
     </div>
 );
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,11 @@
+import React from 'react';
+import { Link, Router } from "react-router-dom";
+
 const Home = () => {
-    return <>Home</>
+    return <>
+    <Link to="/" >Home</Link>
+    <Link to="/results">Results</Link>
+</>
 };
 
 export default Home;
