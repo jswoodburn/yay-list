ed92b745cf2309c0dd4d6c14e3e5c6e23e176eb1
Jackie Woodburn <jackie.woodburn@softwire.com>
2021-12-13 16:29:42 +0000

    Made basic form styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,10 @@
 import React, {useState} from 'react';
+import * as style from '../styles/form.css';
 
 const Form = (
     token
 ) => {
+
     const [sentence, setSetence] = useState('');
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -10,12 +12,22 @@ const Form = (
         alert(`Yay! Your auth token is ${token}}`);
     }
     return (
-        <div>
-            <h1>Yay-List!</h1>
-            <form onSubmit={handleSubmit}>
-                <input type={'text'} name={'sentence'} value={sentence} onChange={e => setSetence(e.target.value)} />
-                <input type={'submit'} value={'Yay!'} />
-            </form>
+        <div className="page">
+            <div className="titleBanner">
+                <h1 className="titleText">Yay-List!</h1>
+            </div>
+            <div className="body">
+                <div className="formBox">
+                    <div className="formTitle">
+                        <h2 className="formTitleText">Every song has a message, and so does every</h2>
+                        <h2 className="inlineYayList">Yay-List!</h2>
+                    </div>
+                    <form className="formField" onSubmit={handleSubmit}>
+                        <input className="formTextField" type={'text'} name={'sentence'} value={sentence} onChange={e => setSetence(e.target.value)} />
+                        <input className="formButton" type={'submit'} value={'Yay!'} />
+                    </form>
+                </div>
+            </div>
         </div>
     )
 };
